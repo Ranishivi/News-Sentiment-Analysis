cd4a22ef048bac9de004e52baf029c95ead9dea4
Shivika <shivikarani2002@gmail.com>
2024-07-26 13:17:38 +0530

    Update views.py

--- news/views.py
+++ news/views.py
@@ -20,8 +20,6 @@ import time
 def home(request):
     return render(request, 'news/home.html')
 
-
-# Data Cleaning
 port_stem = PorterStemmer()
 def stemming(content):
     stemmed_content = re.sub('[^a-zA-Z]', ' ', str(content))
@@ -43,17 +41,24 @@ def analyze_sentiment(article_text):
         return "Neutral"
 
 
-def get_article_sentiment(driver, link):
+def get_article_sentiment(link):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
     driver.get(link)
-    time.sleep(2)  # Allow time for the page to load
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'p')))
     article_soup = BeautifulSoup(driver.page_source, 'html.parser')
     paragraphs = article_soup.find_all('p')
     article_text = ' '.join([p.get_text() for p in paragraphs])
-    stemmed_article = stemming(article_text)[:500]
+    driver.quit()
+    
+    stemmed_article = stemming(article_text)[:300]
     return analyze_sentiment(stemmed_article)
 
 
-def extract_article_data(driver, article):
+def extract_article_data(article):
     try:
         title_tag = article.find('a', {'class': 'JtKRv'})
         if title_tag:
@@ -61,13 +66,12 @@ def extract_article_data(driver, article):
             link = 'https://news.google.com' + title_tag['href'][1:]
             published_time_tag = article.find('time')
             published_time = published_time_tag.text if published_time_tag else 'Unknown'
-            sentiment = get_article_sentiment(driver, link)
-
+            sentiment = get_article_sentiment(link)
 
             return {
                 'title': title,
                 'link': link,
-                'time': published_time,
+                'published_time': published_time,
                 'sentiment': sentiment
             }
         else:
@@ -76,7 +80,7 @@ def extract_article_data(driver, article):
         return None
     
 
-def get_google_news(query, num_articles=12):
+def get_google_news(query, num_articles=15):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
