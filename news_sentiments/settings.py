b8e6d59cb9d793279b74eb7bc29557a20aa5cc6f
Shivika <shivikarani2002@gmail.com>
2024-08-04 01:11:05 +0530

    Update settings.py

--- news_sentiments/settings.py
+++ news_sentiments/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k%q@ko^8y1pc_4uju1_!ovp$znajei+w$z96tg9e1^n)3y&ugh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
